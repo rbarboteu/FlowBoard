b21bf69fd8caeccce5ac164463e8e6a544bbdf9a
rbarboteu <barboteuraphael@gmail.com>
2026-03-07 15:42:52 -0300

    feat: auto migration no startup

--- FlowBoard.API/Program.cs
+++ FlowBoard.API/Program.cs
@@ -63,6 +63,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
